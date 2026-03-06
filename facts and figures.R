c01e6a4856e7baec5560cee160e511e06a9cb80d
leo alejandro <alejomeo@gmail.com>
2026-03-06 13:15:48 -0400

    6 de marzo

--- facts and figures.R
+++ facts and figures.R
@@ -1,5 +1,10 @@
+#install.packages("survey")
+#install.packages("srvyr")
 library(haven)
 library(dplyr)
+library(ggplot2)
+library(survey)
+library(srvyr)
 emp21 <- read_sav("database/empleo/ECE_1T2022.sav")
 emp22 <- read_sav("database/empleo/ECE_2T2022.sav")
 emp23 <- read_sav("database/empleo/ECE_3T2022.sav")
@@ -12,37 +17,51 @@ emp41 <- read_sav("database/empleo/ECE_1T2024.sav")
 emp42 <- read_sav("database/empleo/ECE_2T2024.sav")
 emp43 <- read_sav("database/empleo/ECE_3T2024.sav")
 emp44 <- read_sav("database/empleo/ECE_4T2024.sav")
+emp51 <- read_sav("database/empleo/ECE_1T2025.sav")
+emp52 <- read_sav("database/empleo/ECE_2T2025.sav")
+save(emp21,emp22,emp23,emp24,emp31,emp32,emp33,emp34,emp41,emp42,emp43,emp44,emp51,emp52,
+     file= "database/empleo/emp.RData")
+load("database/empleo/emp.RData")
 
 # factores que afectan al participacion laboral     
 # factores sociales, edicacion, caracteristicas de salud
-View(emp21)
+names(emp41)
+aux1=emp41 %>% select(depto:s1_14esp,pead,peadces,peadasp,s2_08b_a,s2_08b_b,estrato,upm,fact_mes,fact_trim)
 
+aux1$mesb=ifelse(aux1$s2_08b_b==2,aux1$s2_08b_a*0.230137,ifelse(aux1$s2_08b_b==8,aux1$s2_08b_a*12,aux1$s2_08b_a))
 
-table(emp21$s2_08b_a)
-table(emp21$s2_08b_b)
-
-
-nrow(emp21)
-emp$s1_03a
-
-emp21 %>% group_by(s1_03a) %>% count() %>% View()
-
-emp21 %>% group_by(s1_07a) %>% count() %>% View()
-
-m=emp44 %>% filter(s1_07a>=71) %>% nrow() 
-n=emp44 %>% filter(s1_07a>=71, s2_09==2) %>% nrow() 
-n/m*100
-
-m=emp44 %>% filter(s1_03a>=19) %>% nrow() 
-n=emp44 %>% filter(s1_03a>=19,s2_09==2) %>% nrow() 
-n/m*100
-
-emp21 %>% names()
-emp21$s2_08b_b
+aux1$mesagrup = cut(aux1$mesb, 
+                    breaks = c(0,1,3,6,12,24,Inf),
+                    labels = c("< 1 mes", "1-3 meses", "3-6 meses", "6-12 meses", "12-24 meses", "> 24 meses"))
 
 
 
-emp21 %>% filter(s2_05==1) %>% group_by(s2_08b_b,s2_08b_a) %>% count() %>% View()
 
-emp21 %>% filter(s2_05==1) %>% group_by(s2_08b_a) %>% count()
+
+aux1 %>% names()
+aux1 %>% filter(pead==1) %>% nrow()
+aux1 %>% filter(!is.na(s2_08b_a)) %>% nrow()
+
+deg1 = svydesign(id = ~upm,
+          strata = ~estrato,
+          weights = ~fact_trim,
+          data = aux1)
+
+aux1_deg = as_survey(deg1)
+
+distribucion = aux1_deg %>% filter(pead==1,!is.na(mesagrup)) %>% group_by(mesagrup) %>% summarise(n = survey_total())
+
+distribucion = distribucion %>% mutate(p=n/sum(n))
+distribucion = distribucion %>% mutate(F = cumsum(p))
+distribucion
+
+plot(distribucion$F)
+
+hist(distribucion$p,breaks = 10)
+
+
+
+26/4.33
+
+
 
